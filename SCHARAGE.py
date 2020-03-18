cfe5f57cf49672ab32bb971fb23571399c7d0e0c
maciek666 <czerwoneszelki19@gmail.com>
2020-03-18 19:16:42 +0100

    zle wyniki

--- SCHARAGE.py
+++ SCHARAGE.py
@@ -1,3 +1,5 @@
+from math import inf
+
 from RPQ import RPQ
 
 
@@ -8,22 +10,28 @@ class Schrage:
         return RPQ.readData(filepath)
 
     @staticmethod
-    def loss_functroin():
-        return 0
+    def save_min(N):
+        if len(N) < 1:
+            return inf
+        else:
+            return min(N)[0]
 
     @staticmethod
     def schrage(data):
+        """"
+                data - array of arrays [[r,p,q],[r1,p1,q1],......]
+            """
         Pi = []
         k = 0
         G = []
-        d = []
         N = data.copy()
         t = min(N)[0]
         while len(G) != 0 or len(N) != 0:
-            while len(N) != 0 and min(N)[0] <= t:
+            while len(N) != 0 and Schrage.save_min(N) <= t:
                 e = min(N, key=lambda x: x[0])
                 G.append(e)
                 N.remove(e)
+
             if len(G) != 0:
                 e = max(G, key=lambda x: x[2])
                 G.remove(e)
@@ -31,12 +39,13 @@ class Schrage:
                 t = t + e[1]
                 k = k + 1
             else:
-                t = min(N, key=lambda x: x[0])
-
+                t = min(N, key=lambda x: x[0])[0]
         return Pi
 
 
-n, data = RPQ.readData('D:\Programming\python\SPD\data10.txt')
-
-odp = Schrage.schrage(data)
-print(odp)
+tab = [10, 20, 50, 100, 200, 500]
+for i in tab:
+    n, data = RPQ.readData('D:\Programming\python\SPD\data' + str(i) + '.txt')
+    odp = Schrage.schrage(data)
+    times = RPQ.loss_function(odp)
+    print('dla pliku data' + str(i) + '.txt czas to:  ' + str(max(times)))
