43c6230bbe4de31598f085ef3810f43a0d1dc931
maciek666 <czerwoneszelki19@gmail.com>
2020-03-19 12:41:02 +0100

    sortowanie po R i Q

--- RPQ.py
+++ RPQ.py
@@ -33,19 +33,24 @@ class RPQ:
         order_by_access_time.sort(key=lambda x: x[0])
         return order_by_access_time
 
+    @staticmethod
+    def sort_R_and_Q(data):
+        order = data.copy()
+        order.sort(key=lambda x: [x[0], x[2]])
+        # print(order)
+        return order
+
 
 def main():
-    n, data = RPQ.readData('D:\Programming\python\SPD\data10.txt')
-    inoreder = RPQ.sort_R(data)
-    print(inoreder)
-    print(data)
-    sorted = RPQ.loss_function(inoreder)
-
-    not_sorted = RPQ.loss_function(data)
-    print('sort by R')
-    print(sorted)
-    print('\nnon sorted')
-    print(not_sorted)
+    tab = [10, 20, 50, 100, 200, 500]
+    for i in tab:
+        n, data = RPQ.readData('D:\Programming\python\SPD\data' + str(i) + '.txt')
+        odp_r = RPQ.sort_R(data)
+        odp_r_q = RPQ.sort_R_and_Q(data)
+        times_r = RPQ.loss_function(odp_r)
+        times_r_q = RPQ.loss_function(odp_r_q)
+        print('Sortowanie po R dla pliku data' + str(i) + '.txt czas to:  ' + str(max(times_r)))
+        print('Sortowanie po R i Q dla pliku data' + str(i) + '.txt czas to:  ' + str(max(times_r_q)))
 
 
 if __name__ == '__main__':
