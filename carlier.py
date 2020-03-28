b13c66f52bf0021a657edb827c8ef22b04d58785
maciek666 <czerwoneszelki19@gmail.com>
2020-03-29 00:23:23 +0100

    dalej bugi ale lepiej

--- carlier.py
+++ carlier.py
@@ -9,48 +9,49 @@ class Carlier(SchragePmtn):
 
     @staticmethod
     def carlier(data):
-        UB = math.inf
+        UB = [math.inf]
         Pi_star = data.copy()
-        return Carlier.do_carlier(Pi_star, UB)
+        Carlier.do_carlier(Pi_star, UB)
+        return UB[0]
 
     @staticmethod
     def do_carlier(Pi_star, UB):
         Pi = Schrage.schrage_nlogn(Pi_star)
         U = max(RPQ.loss_function(Pi))
-        if U < UB:
-            UB = U
-            Pi_star = Pi
+        if U < UB[0]:
+            UB[0] = U
+
         a, b, c = Carlier.find_a_b_c(Pi)
         if c is None:
-            return Pi_star
+            return
 
         K = Carlier.find_k(Pi, c, b)
         r_ = min(K, key=lambda x: x[0])[0]
         q_ = min(K, key=lambda x: x[2])[2]
         p_ = sum(i for _, i, _ in K)
-
-        # TODO dwie linie poniżej wrażliwe na głupte
         #######################
         H = r_ + p_ + q_
         Hc = H + sum(c)
         #######################
+        # TODO dwie linie poniżej wrażliwe na głupte
+
         tmp_r = c[0]
         c[0] = max(c[0], r_ + p_)
-        LB = SchragePmtn.schrage_nlogn_pmtn(Pi_star)
-        LB = max(H, Hc, LB)
-        if LB < UB:
-            Carlier.do_carlier(Pi_star, UB)
-        c[0] = tmp_r
 
+        LB = SchragePmtn.schrage_nlogn_pmtn(Pi)
+        # LB = max(H, Hc, LB)
+        if LB < UB[0]:
+            Carlier.do_carlier(Pi, UB)
+
+        c[0] = tmp_r
         #######################
         tmp_q = c[2]
         c[2] = max(c[2], q_ + p_)
-        LB = SchragePmtn.schrage_nlogn_pmtn(Pi_star)
-        LB = max(H, Hc, LB)
-        if LB < UB:
-            Carlier.do_carlier(Pi_star, UB)
+        LB = SchragePmtn.schrage_nlogn_pmtn(Pi)
+        # LB = max(H, Hc, LB)
+        if LB < UB[0]:
+            Carlier.do_carlier(Pi, UB)
         c[2] = tmp_q
-        return Pi_star
 
     #####################################################
     ################ HELP METHODS #######################
@@ -67,12 +68,15 @@ class Carlier(SchragePmtn):
         # sposob czytelniejszy niz fory ale bardziej zlozony o zipowanie
 
         time = 0
-        Cmax = max(RPQ.loss_function(Pi))
+        C = RPQ.loss_function(Pi)
+        Cmax = max(C)
+
+        # print(Carlier.loss_function(Pi))
         b = None
         for task in Pi:
             time = max(time, task[0]) + task[1]
             if time + task[2] == Cmax:
-                b = task
+                b = task  # ostatni czyli największy
 
         a = None
         p_accumulation = 0
@@ -88,6 +92,16 @@ class Carlier(SchragePmtn):
             p_accumulation = p_accumulation - task[1]
             if task == b:
                 break
+
+        # for task in Pi:
+        #     s = 0
+        #     for i in range(Pi.index(task), Pi.index(b) + 1):
+        #         s = s + Pi[i][1]
+        #     if s + task[0] + b[2] == Cmax:
+        #         x = (task == a)
+        #         print(x)
+        #         break
+        # poprawne
 
         c = None
         for task_index in range(Pi.index(a), Pi.index(b) + 1):
@@ -112,10 +126,9 @@ class Carlier(SchragePmtn):
 
 if __name__ == '__main__':
     tab = [10, 20, 50, 100, 200, 500]
+    t = [5]
     for i in tab:
         n, data = Carlier.read_data('D:\Programming\python\SPD\data' + str(i) + '.txt')
-        Pi = Schrage.schrage_nlogn(data)
-        Carlier.find_a_b_c(Pi)
         odp = Carlier.carlier(data)
-        t = RPQ.loss_function(odp)
-        print("carlier dla " + str(i) + ' pliku wynik to: ' + str(max(t)))
+
+        print("carlier dla " + str(i) + ' pliku wynik to: ' + str(odp))
