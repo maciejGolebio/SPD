1f48a3045726fc53bf33e48a261cc528e6276ad4
maciek666 <czerwoneszelki19@gmail.com>
2020-04-30 17:04:51 +0200

    alg johnsona dla 2 maszyn.

---
+++ fsp/johnson.py
@@ -0,0 +1,34 @@
+import copy as cp
+import numpy as np
+
+from fsp import fsp_service
+
+
+class Johnson:
+    @staticmethod
+    def find_cmax(data):
+        l: int = 0
+        k: int = len(data) - 1
+        N = cp.deepcopy(data)
+        Pi = [None] * (k + 1)
+        j = None
+        while len(N) > 0:
+            x = np.where(N == np.amin(N))
+            j, i = np.unravel_index(np.argmin(N, axis=None), np.shape(N))
+            if N[j][0] < N[j][1]:
+                Pi[l] = N[j]
+                l = l + 1
+            else:
+                Pi[k] = N[j]
+                k = k - 1
+
+            del N[j]
+
+        return Pi
+
+
+_, m, data = fsp_service.FSPService().read_data('D:\Programming\python\SPD\\fsp\data\data001.txt')
+Pi = Johnson.find_cmax(data)
+cmax = fsp_service.FSPService.loss_function(Pi, 2)
+print(Pi)
+print(cmax)
