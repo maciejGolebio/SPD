6e36f0b7ec904cae71ecdbf21709eea5f7c24419
maciek666 <czerwoneszelki19@gmail.com>
2020-05-30 13:05:03 +0200

    implementacja interfacu cd

--- simulated_annealing/sa.py
+++ simulated_annealing/sa.py
@@ -10,24 +10,25 @@ import copy as cp
 class SimulatedAnnealing:
     @staticmethod
     def procedure(J, n, m, L, T, reduce_temp_fcn, arg_reduce_temp, init_solution_fcn, move_fcn):
-        T_end = None
+        T_end = 0.05
         pi_new = None
-        pi_star = None
+        pi_star = init_solution_fcn(J, m)
         pi = init_solution_fcn(J, m)
         while T > T_end:
             for k in range(L):
-                i = random.randint(0, n)
-                j = random.randint(0, n)
-                pi_new = move_fcn(pi, i, j)
-                delta_C_max = abs(FSPService.loss_function(pi_new, m) - FSPService.loss_function(pi, m))
-                if FSPService.loss_function(pi_new, m) > FSPService.loss_function(pi, m):
+                i = random.randint(0, n - 1)
+                j = random.randint(0, n - 1)
+                pi_new = move_fcn(pi_star, i, j)
+                delta_C_max = FSPService.loss_function(pi, m) - FSPService.loss_function(pi_new, m)
+                if delta_C_max > 0:
                     r = random.uniform(0, 1)
-                    if r >= delta_C_max / T:
+                    if r >= np.e ** (delta_C_max / T):
                         pi_new = cp.copy(pi)
                 pi = pi_new
-                if FSPService.loss_function(pi, m) < FSPService.loss_function(pi, m):
+                if FSPService.loss_function(pi, m) < FSPService.loss_function(pi_star, m):
                     pi_star = cp.copy(pi)
             T = reduce_temp_fcn(T, arg_reduce_temp)
+        print(FSPService.loss_function(pi, m))
 
     @staticmethod
     def init_solution_1(J, m):
@@ -56,6 +57,12 @@ class SimulatedAnnealing:
 
     @staticmethod
     def move_adjacent_swap(Pi, i, j):
+        """
+        :param Pi:
+        :param i: <0,len(Pi)-2>
+        :param j: -------------
+        :return: better neighbour
+        """
         pass
 
     @staticmethod
@@ -73,8 +80,10 @@ class SimulatedAnnealing:
 
 
 if __name__ == '__main__':
-    filepath = 'D:\Programming\python\SPD\\neh\dane\\ta001.txt'
+    filepath = 'D:\Programming\python\SPD\\neh\dane\\ta003.txt'
     n, m, data = NEHService.read_data_by_rows(filepath)
+    natural_c_max = FSPService.loss_function(data, m)
+    print(f'natural Cmax = {natural_c_max}')
     L = [10 ** 2, 10 ** 3, 10 ** 4]
     T_0 = [math.sqrt(n), n, n ** 2]
     temp_reduce_fcn_arr = [SimulatedAnnealing.reduce_temperature_linear,
@@ -91,7 +100,7 @@ if __name__ == '__main__':
                     SimulatedAnnealing.move_adjacent_swap]
     SimulatedAnnealing.procedure(data, n, m,
                                  L[0],
-                                 T_0[0],
+                                 T_0[1],
                                  temp_reduce_fcn_arr[0],
                                  x[0],
                                  init_solution_fcn_arr[0],
